d630cea699b38c05949bf9377af62760c7cda7f0
SirKittyton <gregcrutchlow@gmail.com>
2022-12-02 10:21:48 -0500

    practicing basic JS

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-const massMark = 78
-const heightMark = 1.69
-const massJohn = 92
-const heightJohn = 1.95
+const massMark = 95
+const heightMark = 1.88
+const massJohn = 85
+const heightJohn = 1.76
 
 const bmiMark = massMark / heightMark ** 2
 const bmiJohn = massJohn / heightJohn ** 2
 
-console.log(bmiMark);
-console.log(bmiJohn);
+// console.log(bmiMark, bmiJohn);
 
-const markHigherBMI = bmiMark >= bmiJohn
 
-console.log(markHigherBMI);
+// const markHigherBMI = bmiMark >= bmiJohn
+
+// console.log(markHigherBMI);
+
+// const age = 15;
+
+// if (age >= 18) {
+// 	console.log("Sarah can start driving her license 🚗")
+// } else {
+//     const yearsLeft = 18 - age;
+// 	console.log(`Sarach cannot start driving yet 😢. Wait another ${yearsLeft} years.`)
+// }
+
+// const birthYear = 2012;
+
+// let century;
+// if (birthYear <= 2000) {
+//     century = 20;
+// } else {
+//     century = 21;
+// }
+// console.log(century);
+
+if (bmiMark >= bmiJohn) {
+    console.log(`Mark's BMI is higher! Mark's BMI is ${bmiMark}.`);
+} else {
+    console.log(`John's BMI is higher! John's BMI is ${bmiJohn}`);
+}
