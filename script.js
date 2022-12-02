822e6a5bb9aac224db1b4a05fa45621ab99538a1
SirKittyton <gregcrutchlow@gmail.com>
2022-12-02 11:20:57 -0500

    more practice

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const massMark = 95
-const heightMark = 1.88
-const massJohn = 85
-const heightJohn = 1.76
+// const massMark = 95
+// const heightMark = 1.88
+// const massJohn = 85
+// const heightJohn = 1.76
 
-const bmiMark = massMark / heightMark ** 2
-const bmiJohn = massJohn / heightJohn ** 2
+// const bmiMark = massMark / heightMark ** 2
+// const bmiJohn = massJohn / heightJohn ** 2
 
 // console.log(bmiMark, bmiJohn);
 
@@ -32,8 +32,23 @@ const bmiJohn = massJohn / heightJohn ** 2
 // }
 // console.log(century);
 
-if (bmiMark >= bmiJohn) {
-    console.log(`Mark's BMI is higher! Mark's BMI is ${bmiMark}.`);
+// if (bmiMark >= bmiJohn) {
+//     console.log(`Mark's BMI is higher! Mark's BMI is ${bmiMark}.`);
+// } else {
+//     console.log(`John's BMI is higher! John's BMI is ${bmiJohn}`);
+// }
+
+const scoreDolphins = (97 + 112 + 101) / 3
+const scoreKoalas = (109 + 95 + 106) / 3
+
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins > 100) {
+    console.log(`Dolphins win the tournament with an average score of ${scoreDolphins} to ${scoreKoalas}!`);
+} else if (scoreDolphins < scoreKoalas && scoreKoalas > 100){
+    console.log(`Koalas win the tournament with an average score of ${scoreKoalas} to ${scoreDolphins}`);
+} else if (scoreDolphins === scoreKoalas) {
+    console.log("Its a draw!");
 } else {
-    console.log(`John's BMI is higher! John's BMI is ${bmiJohn}`);
+    console.log(`There is no winner, it was a draw!`);
 }
